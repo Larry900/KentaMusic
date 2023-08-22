f29426ea8fc0192fe5dc3140c2f2c78dd051ed79
Larry900 <142889038+Larry900@users.noreply.github.com>
2023-08-22 22:18:19 +0700

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -14,7 +14,7 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
-from AlexsaceiMusic import app
+from AlexaMusic import app
 
 
 def start_pannel(_):
